451c12ba50e3ba0b7fb5e85dd796a62ea4146804
James Latten <billing@sitelyftstudios.com>
2019-03-24 20:14:00 -0400

    sidebar link fix

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -13,7 +13,7 @@
                         <a class="" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
                     </li>
                 <?php } else { ?>
-                    <li itemprop='url' title="Learn who we are and what we do"><a itemprop='name' href="<?php echo url("/"); ?>/about">About Us</a></li>
+                    <li itemprop='url' title="Learn who we are and what we do"><a itemprop='name' href="<?php echo url("/"); ?>/about_us">About Us</a></li>
                     <li itemprop='url' title="Learn about our services that we provide"><a itemprop='name' href="<?php echo url("/"); ?>/login">Login</a></li>
                     <li class="special"><a class="" href="<?php echo url("/"); ?>/register">Register</a></li>
                 <?php } ?>
